6a5645a8df5a1da49e40cc8d6d356c876bda4b89
Lazaro Francisco Juarez <lazarojuarez.pa@gmail.com>
2024-08-02 16:21:25 -0600

    campos de la tabla de Formacion Docente

--- app/Models/Formacion.php
+++ app/Models/Formacion.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Formacion extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name_curso',
+        'categoria',
+        'tema',
+        'fech_inicio',
+        'fech_fin',
+        'horario',
+        'name_instructor',
+
+    ];
 }
