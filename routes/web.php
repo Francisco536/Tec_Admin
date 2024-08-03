b909e01c6628001b7435f522adbfef09d06af295
Lazaro Francisco Juarez <lazarojuarez.pa@gmail.com>
2024-08-03 10:26:59 -0600

    Rutas admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,4 +15,16 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+
+    Route::group(['prefix' => 'admin'], function(){
+        Route::get('/index', [AdminController::class, 'index'])->name('lista.admin')->middleware('auth');
+        Route::get('/{id}/show', [AdminController::class, 'show'])->name('ver.admin')->middleware('auth');
+        Route::get('/create', [AdminController::class, 'create'])->name('add.admin')->middleware('auth');
+        Route::post('/store', [AdminController::class, 'store'])->name('store.admin')->middleware('auth');
+        Route::get('/{id}/edit', [AdminController::class, 'edit'])->name('edit.admin')->middleware('auth');
+        Route::post('/update/{id}', [AdminController::class, 'update'])->name('update.admin')->middleware('auth');
+        Route::get('destroy/{admin}', [AdminController::class, 'destroy'])->name("destroy.admin")->middleware('auth');
+
+    });
 });
